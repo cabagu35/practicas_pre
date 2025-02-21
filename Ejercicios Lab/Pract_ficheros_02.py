93b895182232f391383526a4afcdbfb3a6254dcf
pol_cabezas <cabagu35@gmail.com>
2025-02-21 14:00:57 +0100

    Practica Ficheros 14:00

---
+++ Ejercicios Lab/Pract_ficheros_02.py
@@ -0,0 +1,13 @@
+#Ejercicio 2: El Invitado
+
+#Función
+def reg_invitados(archivo):
+    with open(archivo, 'a') as invitados:
+        nombre = input("¿Cómo se llama el nuevo invitado?: ")
+        invitados.write(nombre+'\n')
+
+
+
+#Programa principal:
+archivo = 'invitados.txt'
+reg_invitados(archivo)
--- prueba.py
+++
@@ -1,1 +0,0 @@
-#Arhchivo de prueba
